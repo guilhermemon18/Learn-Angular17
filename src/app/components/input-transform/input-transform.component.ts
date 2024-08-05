a3f66acedb51071e469f65a563c32e64398a7bea
guilhermemon18 <guilhermemonsani7@gmail.com>
2024-08-05 15:49:57 -0300

    Aprendendo a utilizar input transformations

--- src/app/components/input-transform/input-transform.component.ts
+++ src/app/components/input-transform/input-transform.component.ts
@@ -1,6 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { User } from '../../app.component';
 
+function setUserNameToUpperCase(user: User): User {
+  return {
+    ...user,
+    name: user.name.toUpperCase(),
+  };
+}
+
 @Component({
   selector: 'app-input-transform',
   standalone: true,
@@ -12,5 +19,6 @@ import { User } from '../../app.component';
   `,
 })
 export class InputTransformComponent {
-  @Input({ required: true }) public user!: User;
+  @Input({ required: true, transform: setUserNameToUpperCase })
+  public user!: User;
 }
